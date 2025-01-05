edef710b560ee93c2cb6b04f104d1bb8a208ad13
Filippo <filippo_bissi@outlook.it>
2025-01-05 11:48:41 +0100

    adv

--- source/conf.py
+++ source/conf.py
@@ -19,4 +19,4 @@ html_static_path = ['_static']
 # Adding the path to the Library folder
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../Library/urbanalize.py'))  # Percorso relativo alla cartella "Library"
+sys.path.insert(0, os.path.abspath(r'../../Library/urbanalize.py'))  # Percorso relativo alla cartella "Library"
